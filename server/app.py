d02c7a00108ef6e6e889110cd2092a8c71ea9bf4
Phelix <phelixmbani@gmail.com>
2025-01-24 13:24:36 +0300

    authrnticate user

--- server/app.py
+++ server/app.py
@@ -47,10 +47,39 @@ class ShowArticle(Resource):
             return make_response(article_json, 200)
 
         return {'message': 'Maximum pageview limit reached'}, 401
+    
+class Login(Resource):
+    
+    def post(self):
+        user = User.query.filter(User.username == request.get_json()['username']).first()
+        session['user_id'] = user.id
+        return make_response(user.to_dict(), 200)
+    
+class CheckSession(Resource):
+    
+    def get(self):
+        user = User.query.filter(User.username == session.get('user_id')).first()
+        if user:
+            return user.to_dict()
+        else:
+            return make_response({
+                "message": "401: Not Authorized"
+            }, 401)
+            
+class Logout(Resource):
+    
+    def delete(self):
+        session['user_id'] = None
+        return make_response({
+            "message":"204: No content"
+        }, 204)
 
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
+api.add_resource(CheckSession, '/check_session')
+api.add_resource(Logout, '/logout')
+api.add_resource(Login, '/login')
 
 
 if __name__ == '__main__':
